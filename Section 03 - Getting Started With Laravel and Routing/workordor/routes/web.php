6e2be46cf0cc1ccd20ecb6d38b2c1fa21bf9264e
Ferenc Koler <ferenckoler@outlook.com>
2025-05-10 18:22:51 +0200

    Intro To Routing

--- Section 03 - Getting Started With Laravel and Routing/workordor/routes/web.php
+++ Section 03 - Getting Started With Laravel and Routing/workordor/routes/web.php
@@ -5,3 +5,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+    return '<h1>Available Jobs</h1>';
+})->name('jobs');
+
+// Route::post('/submit', function () {
+//     return 'Submitted';
+// });
+
+// Route::match(['get', 'post'], '/submit', function () {
+//     return 'Submitted';
+// });
+
+Route::any('/submit', function () {
+    return 'Submitted';
+});
+
+Route::get('/test', function () {
+    $url = route('jobs');
+
+    return "<a href='$url'>Click Here</a>";
+});
+
+Route::get('/api/users', function () {
+    return [
+        'name' => 'Alex Skolnick',
+        'email' => 'alex@skolnick.io',
+    ];
+});
+
+// php artisan route:list
