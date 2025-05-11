6d893bd4b484ec43568a2267ff7af81e88b54b6b
Ferenc Koler <ferenckoler@outlook.com>
2025-05-11 21:33:17 +0200

    Response Helper - Header, Json, Download, Cookies and Read Cookie

--- Section 03 - Getting Started With Laravel and Routing/workordor/routes/web.php
+++ Section 03 - Getting Started With Laravel and Routing/workordor/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,14 +13,32 @@ Route::get('/jobs', function () {
 
 Route::get('/abq', function () {
     return response(
-        'Representing the ABQ. What up biat**?! Leave at the tone.',
+        '<h1>Representing the ABQ. What up biat**?! Leave at the tone.</h1>',
         200
-    );
+    )->header('Content-Type', 'text/html');
 });
 
-Route::get('/zed', function () {
-    return response(
-        'Zed is dead baby',
-        404
-    );
+// Route::get('/abq/json', function () {
+//     return response()->json(['abq' => 'Representing the ABQ. What up biat**?! Leave at the tone.']);
+// });
+
+Route::get('/download', function () {
+    return response()->download(public_path('favicon.ico'));
 });
+
+Route::get('/abq/json', function () {
+    return response()->json(['abq' => 'Representing the ABQ. What up biat**?! Leave at the tone.'])->cookie('name', 'Jesse Pinkman');
+});
+
+Route::get('read-cookie', function (Request $request) {
+    $cookieValue = $request->cookie('name');
+
+    return response()->json(['cookie' => $cookieValue]);
+});
+
+// Route::get('/zed', function () {
+//     return response(
+//         'Zed is dead baby',
+//         404
+//     );
+// });
