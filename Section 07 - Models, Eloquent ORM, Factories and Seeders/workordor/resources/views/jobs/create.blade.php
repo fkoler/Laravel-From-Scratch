101757ecf552b4c1935c6e64896b1fb61bd9e1e3
Ferenc Koler <ferenckoler@outlook.com>
2025-06-04 23:19:13 +0200

    Input Validation & Errors - Error Messages

--- Section 07 - Models, Eloquent ORM, Factories and Seeders/workordor/resources/views/jobs/create.blade.php
+++ Section 07 - Models, Eloquent ORM, Factories and Seeders/workordor/resources/views/jobs/create.blade.php
@@ -4,10 +4,21 @@
 
     <form action="/jobs" method="POST">
         @csrf
-        <input class="bg-blue-700 w-full md:w-72 px-4 py-3 focus:outline-none" type=text name="title"
-            placeholder="title">
-        <input class="bg-blue-700 w-full md:w-72 px-4 py-3 focus:outline-none" type=text name="description"
-            placeholder="description">
+        <div class="my-5">
+            <input class="bg-blue-700 w-full md:w-72 px-4 py-3 focus:outline-none" type=text name="title"
+                placeholder="title" value="{{ old('title') }}">
+            @error('title')
+                <div class="text-red-500 mt-2 text-sm">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="mb-5">
+            <input class="bg-blue-700 w-full md:w-72 px-4 py-3 focus:outline-none" type=text name="description"
+                placeholder="description" value="{{ old('description') }}">
+            @error('description')
+                <div class="text-red-500 mt-2 text-sm">{{ $message }}</div>
+            @enderror
+        </div>
 
         <button
             class="bg-blue-500 hover:bg-blue-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300 cursor-pointer"
