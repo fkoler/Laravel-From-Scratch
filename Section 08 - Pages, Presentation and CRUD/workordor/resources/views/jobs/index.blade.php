e3cad83a1ee2ebdc5a3dfc3ba674ac6777f71daa
Ferenc Koler <ferenckoler@outlook.com>
2025-06-16 23:52:03 +0200

    Homepage Jobs

--- Section 08 - Pages, Presentation and CRUD/workordor/resources/views/jobs/index.blade.php
+++ Section 08 - Pages, Presentation and CRUD/workordor/resources/views/jobs/index.blade.php
@@ -1,5 +1,4 @@
 <x-layout>
-    <x-slot name="title">Available Jobs</x-slot>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
         @forelse ($jobs as $job)
             <x-job-card :job="$job" />
--- Section 08 - Pages, Presentation and CRUD/workordor/resources/views/pages/index.blade.php
+++ Section 08 - Pages, Presentation and CRUD/workordor/resources/views/pages/index.blade.php
@@ -1,5 +1,16 @@
 <x-layout>
-    <h1>Welome To Workordor</h1>
+    <h2 class="text-center text-3xl mb-7 font-bold border border-gray-900 p-3">Welome To Workordor</h2>
 
-    <x-bottom-banner></x-bottom-banner>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
+        @forelse ($jobs as $job)
+            <x-job-card :job="$job" />
+        @empty
+            <p>No Jobs Available</p>
+        @endforelse
+    </div>
+
+    <a href="{{ route('jobs.index') }}" class="block text-xl text-center">
+        <i class="fa fa-arrow-alt-circle-right"></i> Show All Jobs
+    </a>
+    <x-bottom-banner />
 </x-layout>
