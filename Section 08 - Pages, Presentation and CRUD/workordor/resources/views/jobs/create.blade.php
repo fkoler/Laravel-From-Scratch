c7210d643f9081221f5e439d1d3ccf83d3e02076
Ferenc Koler <ferenckoler@outlook.com>
2025-06-18 19:54:09 +0200

    Create Job Page

--- Section 08 - Pages, Presentation and CRUD/workordor/resources/views/jobs/create.blade.php
+++ Section 08 - Pages, Presentation and CRUD/workordor/resources/views/jobs/create.blade.php
@@ -1,27 +1,190 @@
 <x-layout>
     <x-slot name="title">Create New Job</x-slot>
-    <h1>Create New Job</h1>
 
-    <form action="/jobs" method="POST">
-        @csrf
-        <div class="my-5">
-            <input class="bg-blue-700 w-full md:w-72 px-4 py-3 focus:outline-none" type=text name="title"
-                placeholder="title" value="{{ old('title') }}">
-            @error('title')
-                <div class="text-red-500 mt-2 text-sm">{{ $message }}</div>
-            @enderror
+    <main class="container mx-auto p-4 mt-4">
+        <div class="bg-blue-900 mx-auto p-8 rounded-lg shadow-md w-full md:max-w-3xl">
+            <h2 class="text-4xl text-center font-bold mb-4">
+                Create Job Listing
+            </h2>
+
+            <form method="POST" action="/jobs" enctype="multipart/form-data">
+
+                {{-- Job Info --}}
+                @csrf
+                <h2 class="text-2xl font-bold mb-6 text-center text-blue-200">
+                    Job Info
+                </h2>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="title">Job Title</label>
+                    <input id="title" type="text" name="title"
+                        class="w-full px-4 py-2 border rounded focus:outline-none 
+                        @error('title')
+                            border-red-500                            
+                        @enderror"
+                        placeholder="Web Developer" value="{{ old('title') }}" />
+                    @error('title')
+                        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="description">Job Description</label>
+                    <textarea cols="30" rows="7" id="description" name="description"
+                        class="w-full px-4 py-2 border rounded focus:outline-none
+                        @error('description')
+                            border-red-500                            
+                        @enderror"
+                        placeholder="We are seeking...">{{ old('description') }}</textarea>
+                    @error('description')
+                        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="salary">Annual Salary</label>
+                    <input id="salary" type="text" name="salary"
+                        class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="1059714" />
+                </div>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="requirements">Requirements</label>
+                    <textarea id="requirements" name="requirements" class="w-full px-4 py-2 border rounded focus:outline-none"
+                        placeholder="Bachelor's degree..."></textarea>
+                </div>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="benefits">Benefits</label>
+                    <textarea id="benefits" name="benefits" class="w-full px-4 py-2 border rounded focus:outline-none"
+                        placeholder="Health insurance..."></textarea>
+                </div>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="tags">Tags (comma-separated)</label>
+                    <input id="tags" type="text" name="tags"
+                        class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="webdev, js, php" />
+                </div>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="job_type">Job Type</label>
+                    <select id="job_type" name="job_type"
+                        class="bg-blue-900 w-full px-4 py-2 border rounded focus:outline-none
+                        @error('job_type')
+                            border-red-500                            
+                        @enderror">
+                        <option value="Full-Time" {{ old('job_type') === 'Full-Time' ? 'selected' : '' }}>
+                            Full-Time
+                        </option>
+                        <option value="Part-Time" {{ old('job_type') === 'Part-Time' ? 'selected' : '' }}>
+                            Part-Time
+                        </option>
+                        <option value="Contract" {{ old('job_type') === 'Contract' ? 'selected' : '' }}>
+                            Contract
+                        </option>
+                        <option value="Temporary" {{ old('job_type') === 'Temporary' ? 'selected' : '' }}>
+                            Temporary
+                        </option>
+                        <option value="Internship" {{ old('job_type') === 'Internship' ? 'selected' : '' }}>
+                            Internship
+                        </option>
+                        <option value="Volunteer" {{ old('job_type') === 'Volunteer' ? 'selected' : '' }}>
+                            Volunteer
+                        </option>
+                        <option value="On-Call" {{ old('job_type') === 'On-Call' ? 'selected' : '' }}>
+                            On-Call
+                        </option>
+                    </select>
+                    @error('job_type')
+                        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="remote">Remote</label>
+                    <select id="remote" name="remote"
+                        class="bg-blue-900 w-full px-4 py-2 border rounded focus:outline-none">
+                        <option value="false">No</option>
+                        <option value="true">Yes</option>
+                    </select>
+                </div>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="address">Address</label>
+                    <input id="address" type="text" name="address"
+                        class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="123..." />
+                </div>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="city">City</label>
+                    <input id="city" type="text" name="city"
+                        class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="Toronto" />
+                </div>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="state">State</label>
+                    <input id="state" type="text" name="state"
+                        class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="ON" />
+                </div>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="zipcode">ZIP Code</label>
+                    <input id="zipcode" type="text" name="zipcode"
+                        class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="M5V 0G9" />
+                </div>
+
+                {{-- Company Info --}}
+                <h2 class="text-2xl font-bold mb-6 text-center text-blue-200">
+                    Company Info
+                </h2>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="company_name">Company Name</label>
+                    <input id="company_name" type="text" name="company_name"
+                        class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="Company name" />
+                </div>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="company_description">Company Description</label>
+                    <textarea id="company_description" name="company_description"
+                        class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="Company Description"></textarea>
+                </div>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="company_website">Company Website</label>
+                    <input id="company_website" type="text" name="company_website"
+                        class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="Enter website" />
+                </div>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="contact_phone">Contact Phone</label>
+                    <input id="contact_phone" type="text" name="contact_phone"
+                        class="w-full px-4 py-2 border rounded focus:outline-none" placeholder="Enter phone" />
+                </div>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="contact_email">Contact Email</label>
+                    <input id="contact_email" type="email" name="contact_email"
+                        class="w-full px-4 py-2 border rounded focus:outline-none"
+                        placeholder="Email where you want to receive applications" />
+                </div>
+
+                <div class="mb-4">
+                    <label class="block text-gray-300" for="company_logo">Company Logo</label>
+                    <input id="company_logo" type="file" name="company_logo"
+                        class="text-gray-300 w-full px-4 py-2 border rounded focus:outline-none cursor-pointer
+                        @error('company_logo')
+                            border-red-500                            
+                        @enderror" />
+                    @error('company_logo')
+                        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <button type="submit"
+                    class="w-full bg-green-500 hover:bg-green-600 text-white font-bold px-4 py-2 my-3 rounded focus:outline-none cursor-pointer">
+                    Save
+                </button>
+            </form>
         </div>
-
-        <div class="mb-5">
-            <input class="bg-blue-700 w-full md:w-72 px-4 py-3 focus:outline-none" type=text name="description"
-                placeholder="description" value="{{ old('description') }}">
-            @error('description')
-                <div class="text-red-500 mt-2 text-sm">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <button
-            class="bg-blue-700 hover:bg-blue-600 text-white px-4 py-2 rounded hover:shadow-md transition duration-300 cursor-pointer"
-            type="submit">Submit</button>
-    </form>
+    </main>
 </x-layout>
