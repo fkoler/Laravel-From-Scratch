ac0ba447aaf77395b2f6e24d218dc4082aa0c0c4
Ferenc Koler <ferenckoler@outlook.com>
2025-06-27 00:53:33 +0200

    Middleware Overview - Append Middleware LogRequest

--- Section 10 - Middleware, Authorization and Policies/workordor/app/Http/Middleware/LogRequest.php
+++ Section 10 - Middleware, Authorization and Policies/workordor/app/Http/Middleware/LogRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class LogRequest
@@ -15,7 +16,7 @@ class LogRequest
      */
     public function handle(Request $request, Closure $next): Response
     {
-        print('From the LogRequest middleware');
+        Log::info("{$request->method()} - {$request->fullUrl()}");
 
         return $next($request);
     }
