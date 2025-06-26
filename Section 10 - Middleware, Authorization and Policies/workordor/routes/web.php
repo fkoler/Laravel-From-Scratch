5715c3fda468e698df0fb71f94c47ba7fb2ee147
Ferenc Koler <ferenckoler@outlook.com>
2025-06-27 01:01:24 +0200

    Middleware Overview - Using LogRequest For Specific Routes

--- Section 10 - Middleware, Authorization and Policies/workordor/routes/web.php
+++ Section 10 - Middleware, Authorization and Policies/workordor/routes/web.php
@@ -1,19 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 
+use App\Http\Middleware\LogRequest;
+
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::resource('jobs', JobController::class);
 
-Route::get('/register', [RegisterController::class, 'register'])->name('register');
+Route::get('/register', [RegisterController::class, 'register'])->name('register')->middleware(LogRequest::class);
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 
-Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::get('/login', [LoginController::class, 'login'])->name('login')->middleware(LogRequest::class);
 Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
 
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
