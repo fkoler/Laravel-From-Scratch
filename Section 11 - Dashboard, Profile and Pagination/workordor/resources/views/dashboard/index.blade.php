bdddcddf3a7889ebc7973f9145dd2a9f7e513292
Ferenc Koler <ferenckoler@outlook.com>
2025-07-05 22:10:28 +0200

    Profile Controller & Info Update

--- Section 11 - Dashboard, Profile and Pagination/workordor/resources/views/dashboard/index.blade.php
+++ Section 11 - Dashboard, Profile and Pagination/workordor/resources/views/dashboard/index.blade.php
@@ -1,35 +1,59 @@
 <x-layout>
-    <div class="p-8 w-full">
-        <h3 class="text-3xl text-center font-bold mb-4">
-            My Job Listings
-        </h3>
+    <section class="flex flex-col md:flex-row gap-4">
+        {{-- Profile Info Form --}}
+        <div class="p-8 w-full">
+            <h3 class="text-3xl text-center font-bold mb-4">
+                Profile Info
+            </h3>
 
-        @forelse($jobs as $job)
-            <div class="flex justify-between items-center">
-                <div>
-                    <h3 class="text-xl font-semibold">{{ $job->title }}</h3>
+            <form method="POST" action="{{ route('profile.update') }}" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <x-inputs.text id="name" name="name" label="Name" value="{{ $user->name }}" />
+                <x-inputs.text id="email" name="email" label="Email Address" type="email"
+                    value="{{ $user->email }}" />
 
-                    <p class="text-gray-500">{{ $job->job_type }}</p>
+                <button type="submit"
+                    class="w-full bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded focus:outline-none cursor-pointer">
+                    Save
+                </button>
+            </form>
+        </div>
+        {{-- Job Listings --}}
+        <div class="p-8 w-full">
+            <h3 class="text-3xl text-center font-bold mb-4">
+                My Job Listings
+            </h3>
+
+            @forelse($jobs as $job)
+                <div class="flex justify-between items-center">
+                    <div>
+                        <h3 class="text-xl font-semibold">{{ $job->title }}</h3>
+
+                        <p class="text-gray-500">{{ $job->job_type }}</p>
+                    </div>
+
+                    <div class="flex space-x-3">
+                        <a href="{{ route('jobs.edit', $job->id) }}"
+                            class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Edit</a>
+                        {{-- Delete Form --}}
+                        <form method="POST" action="{{ route('jobs.destroy', $job->id) }}?from=dashboard"
+                            onsubmit="return confirm('Are you sure that you want to delete this job?')">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit"
+                                class="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded cursor-pointer">
+                                Delete
+                            </button>
+                        </form>
+                        {{-- End Delete Form --}}
+                    </div>
                 </div>
+            @empty
+                <p class="text-gray-700">You have not job listings</p>
+            @endforelse
+        </div>
+    </section>
 
-                <div class="flex space-x-3">
-                    <a href="{{ route('jobs.edit', $job->id) }}"
-                        class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Edit</a>
-                    {{-- Delete Form --}}
-                    <form method="POST" action="{{ route('jobs.destroy', $job->id) }}?from=dashboard"
-                        onsubmit="return confirm('Are you sure that you want to delete this job?')">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit"
-                            class="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded cursor-pointer">
-                            Delete
-                        </button>
-                    </form>
-                    {{-- End Delete Form --}}
-                </div>
-            </div>
-        @empty
-            <p class="text-gray-700">You have not job listings</p>
-        @endforelse
-    </div>
+    <x-bottom-banner />
 </x-layout>
--- Section 11 - Dashboard, Profile and Pagination/workordor/routes/web.php
+++ Section 11 - Dashboard, Profile and Pagination/workordor/routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 
 use App\Http\Middleware\LogRequest;
 
@@ -27,5 +28,7 @@ Route::middleware('guest')->group(function () {
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+
+Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update')->middleware('auth');
 
 // php artisan route:list
