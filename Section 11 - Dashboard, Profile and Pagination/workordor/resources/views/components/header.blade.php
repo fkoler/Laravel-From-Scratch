cd29d9c7ba09d42ed1a2763526afa32a27c3d07e
Ferenc Koler <ferenckoler@outlook.com>
2025-07-07 18:49:42 +0200

    Show Avatar In Header

--- Section 11 - Dashboard, Profile and Pagination/workordor/resources/views/components/header.blade.php
+++ Section 11 - Dashboard, Profile and Pagination/workordor/resources/views/components/header.blade.php
@@ -6,15 +6,26 @@
         </h1>
 
         <nav class="hidden md:flex items-center space-x-4">
-            <x-nav-link url="/jobs" :active="request()->is('jobs')">All Jobs</x-nav-link>            
-            @auth 
-            <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')">Saved Jobs</x-nav-link>            
-            <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge">Dashboard</x-nav-link>
-            <x-logout-button />
-            <x-button-link url="/jobs/create" icon="edit">Create Job</x-button-link>
+            <x-nav-link url="/jobs" :active="request()->is('jobs')">All Jobs</x-nav-link>
+            @auth
+                <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')">Saved Jobs</x-nav-link>
+                <x-logout-button />
+
+                <div class="flex items-center space-x-3">
+                    <a href="{{ route('dashboard') }}">
+                        @if (Auth::user()->avatar)
+                            <img src="{{ asset('storage/' . Auth::user()->avatar) }}" alt="{{ Auth::user()->name }}"
+                                class="w-10 h-10 rounded-full">
+                        @else
+                            <img src="{{ asset('images/sample_avatar.png') }}" alt="{{ Auth::user()->name }}"
+                                class="w-10 h-10 rounded-full">
+                        @endif
+                    </a>
+                </div>
+                <x-button-link url="/jobs/create" icon="edit">Create Job</x-button-link>
             @else
-            <x-nav-link url="/login" :active="request()->is('login')" icon="user">Login</x-nav-link>
-            <x-nav-link url="/register" :active="request()->is('register')">Register</x-nav-link>
+                <x-nav-link url="/login" :active="request()->is('login')" icon="user">Login</x-nav-link>
+                <x-nav-link url="/register" :active="request()->is('register')">Register</x-nav-link>
             @endauth
         </nav>
 
@@ -28,13 +39,13 @@
         class="md:hidden bg-blue-900 text-white mt-5 pb-3 space-y-2">
         <x-nav-link url="/jobs" :active="request()->is('jobs')" :mobile="true">All Jobs</x-nav-link>
         @auth
-        <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')" :mobile="true">Saved Jobs</x-nav-link>
-        <x-nav-link url="/dashboard" :active="request()->is('dashboard')" :mobile="true">Dashboard</x-nav-link>
-        <x-logout-button />
-        <x-button-link url="/jobs/create" icon="edit" :block="true">Create Job</x-button-link>
-        @else        
-        <x-nav-link url="/login" :active="request()->is('login')" :mobile="true">Login</x-nav-link>
-        <x-nav-link url="/register" :active="request()->is('register')" :mobile="true">Register</x-nav-link>
+            <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')" :mobile="true">Saved Jobs</x-nav-link>
+            <x-nav-link url="/dashboard" :active="request()->is('dashboard')" :mobile="true">Dashboard</x-nav-link>
+            <x-logout-button />
+            <x-button-link url="/jobs/create" icon="edit" :block="true">Create Job</x-button-link>
+        @else
+            <x-nav-link url="/login" :active="request()->is('login')" :mobile="true">Login</x-nav-link>
+            <x-nav-link url="/register" :active="request()->is('register')" :mobile="true">Register</x-nav-link>
         @endauth
     </nav>
 </header>
