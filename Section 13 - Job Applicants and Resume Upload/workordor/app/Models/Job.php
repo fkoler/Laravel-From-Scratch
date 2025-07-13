48e84ee88abaeda605575444ac85ecb5c1fc7a9b
Ferenc Koler <ferenckoler@outlook.com>
2025-07-13 12:32:51 +0200

    Applicant Model

--- Section 13 - Job Applicants and Resume Upload/workordor/app/Models/Job.php
+++ Section 13 - Job Applicants and Resume Upload/workordor/app/Models/Job.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Job extends Model
 {
@@ -53,5 +54,15 @@ class Job extends Model
     public function bokmarkedByUsers(): BelongsToMany
     {
         return $this->belongsToMany(User::class, 'job_user_bookmarks')->withTimestamps();
+    }
+
+    /**
+     * Relation to applicants.
+     *
+     * @return HasMany
+     */
+    public function applicants(): HasMany
+    {
+        return $this->hasMany(Applicant::class);
     }
 }
