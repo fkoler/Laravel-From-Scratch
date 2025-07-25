c31334e05ce608025d895ee9357d3db7858f8adb
Ferenc Koler <ferenckoler@outlook.com>
2025-07-25 22:07:45 +0200

    Search Functionality - Impl

--- Section 14 - Job Search, Maps and Emails/workordor/resources/views/components/search.blade.php
+++ Section 14 - Job Search, Maps and Emails/workordor/resources/views/components/search.blade.php
@@ -1,9 +1,9 @@
 <form method="GET" action="{{ route('jobs.search') }}" class="block mx-5 space-y-2 md:mx-auto md:space-x-2">
     <input type="text" name="keywords" placeholder="Keywords"
-        class="bg-blue-700 w-full md:w-72 px-4 py-3 focus:outline-none" />
+        class="bg-blue-700 w-full md:w-72 px-4 py-3 focus:outline-none" value="{{ request('keywords') }}" />
 
     <input type="text" name="location" placeholder="Location"
-        class="bg-blue-700 w-full md:w-72 px-4 py-3 focus:outline-none" />
+        class="bg-blue-700 w-full md:w-72 px-4 py-3 focus:outline-none" value="{{ request('location') }}" />
 
     <button
         class="w-full md:w-auto bg-blue-700 hover:bg-blue-600 text-white px-4 py-3 focus:outline-none cursor-pointer rounded">
